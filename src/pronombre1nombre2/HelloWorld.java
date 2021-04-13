90308e5a21b6683cae8d898fd26a7ae0914f846b
estefaniahmj <estefania.hurtadom@gmail.com>
2021-04-14 01:23:10 +0200

    Esto es un segundo cambio

--- src/pronombre1nombre2/HelloWorld.java
+++ src/pronombre1nombre2/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo una vez más");
+		System.out.println("Este es el segundo Hola al mundo");
 	}
 
 }
